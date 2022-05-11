298bd3d41a026ff5d3f6bd457d1e4731216afcf3
abhinas regmi <aviregmi2018@gmail.com>
2022-05-11 16:59:21 +0545

    firebase package installed

---
+++ lib/constants/routes.dart
@@ -0,0 +1,6 @@
+const String homeRoute = '/';
+const String loginRoute = '/login/';
+const String registerRoute = '/register/';
+const String kitchenRoute = '/kitchen/';
+const String waiterRoute = '/waiter/';
+const String adminRoute = '/admin/';
---
+++ lib/views/home/admin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AdminView extends StatefulWidget {
+  const AdminView({ Key? key }) : super(key: key);
+
+  @override
+  State<AdminView> createState() => _AdminViewState();
+}
+
+class _AdminViewState extends State<AdminView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
